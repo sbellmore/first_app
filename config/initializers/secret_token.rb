d0c4ef127dbe4acdc4c2ed2cf67db5ba830783d1
sbellmore <shawn.bellmore@ascendlearning.com>
2012-11-12 15:57:29 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5fdee1e2d46f76f0d27ddb1f27b099d41f1754c2bb79609f3cb63898be0b1cc74a9a2fe51ff47c3af490d10fc87ef5a539fc190453dab1f77079b76de9adf43f'
